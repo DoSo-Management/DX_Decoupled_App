a17568d95e3103ca5b0ea988fb8905faa23c8950
Petre Chitashvili <petre@doso.ge>
2018-11-26 15:04:33 +0400

    Fixing a but with SafePostGRESqlConnectionProvider
    Avoiding it, should I say - BUG still persists!

--- DXApplication1.Win/Program.cs
+++ DXApplication1.Win/Program.cs
@@ -9,31 +9,39 @@ using DevExpress.Persistent.Base;
 using DevExpress.Xpo;
 using PostgreSqlConnectionProviderEx;
 
-namespace DXApplication1.Win {
-    static class Program {
+namespace DXApplication1.Win
+{
+    static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
 #if EASYTEST
             DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
 #endif
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
-            if(Tracing.GetFileLocationFromSettings() == DevExpress.Persistent.Base.FileLocation.CurrentUserApplicationDataFolder) {
+            if (Tracing.GetFileLocationFromSettings() == DevExpress.Persistent.Base.FileLocation.CurrentUserApplicationDataFolder)
+            {
                 Tracing.LocalUserAppDataPath = Application.LocalUserAppDataPath;
             }
 
             XpoDefault.TrackPropertiesModifications = true;
-            SafePostgreSqlConnectionProvider.Register();
+            PostgreSqlConnectionProviderCI.Register();
 
             Tracing.Initialize();
             DXApplication1WindowsFormsApplication winApplication = new DXApplication1WindowsFormsApplication();
             // Refer to the https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112680.aspx help article for more details on how to provide a custom splash form.
             //winApplication.SplashScreen = new DevExpress.ExpressApp.Win.Utils.DXSplashScreen("YourSplashImage.png");
-            if(ConfigurationManager.ConnectionStrings["ConnectionString"] != null) {
+            if (ConfigurationManager.ConnectionStrings["ConnectionString"] != null)
+            {
                 winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             }
 #if EASYTEST
@@ -42,17 +50,30 @@ namespace DXApplication1.Win {
             }
 #endif
 #if DEBUG
-            if(System.Diagnostics.Debugger.IsAttached && winApplication.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            if (System.Diagnostics.Debugger.IsAttached && winApplication.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema)
+            {
                 winApplication.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
             }
 #endif
-            try {
+            try
+            {
                 winApplication.Setup();
                 winApplication.Start();
             }
-            catch(Exception e) {
+            catch (Exception e)
+            {
                 winApplication.HandleException(e);
             }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            
+        }
+
+        private static void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            
         }
     }
 }
